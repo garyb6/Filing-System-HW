c5b56a0a9589ed3682708bf09719ba72bced1f86
garyb6 <gary.bennett6@outlook.com>
2022-02-22 22:32:59 +0000

    fodler controller with get and post set up

--- src/main/java/com/filingsystem/filingSystem/controllers/FolderController.java
+++ src/main/java/com/filingsystem/filingSystem/controllers/FolderController.java
@@ -1,4 +1,35 @@
 package com.filingsystem.filingSystem.controllers;
 
+import com.filingsystem.filingSystem.models.File;
+import com.filingsystem.filingSystem.models.Folder;
+import com.filingsystem.filingSystem.repositories.FileRepository;
+import com.filingsystem.filingSystem.repositories.FolderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class FolderController {
+
+    @Autowired
+    FolderRepository folderRepository;
+
+    @GetMapping(value = "/folders")
+    public ResponseEntity<List<Folder>> getAllFolders(){
+        return new ResponseEntity<>(folderRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/folders/{id}")
+    public ResponseEntity getFolder(@PathVariable Long id){
+        return new ResponseEntity(folderRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/folder")
+    public ResponseEntity<Folder> postFolder(@RequestBody Folder folder){
+        folderRepository.save(folder);
+        return new ResponseEntity<>(folder, HttpStatus.CREATED);
+    }
 }
